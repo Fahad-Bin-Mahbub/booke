98098c8ea4a02cdd93bf867abed8baab060756f3
Fahad Bin Mahbub <fahadbm3234@gmail.com>
2026-02-02 23:38:03 +0600

    UI for index added

---
+++ components/ui/index.ts
@@ -0,0 +1,27 @@
+// UI Components - Barrel export
+export { Button } from "./Button";
+export type { ButtonProps, ButtonVariant, ButtonSize } from "./Button";
+
+export { Badge } from "./Badge";
+export type { BadgeProps, BadgeVariant } from "./Badge";
+
+export {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+  CardFooter,
+} from "./Card";
+export type {
+  CardProps,
+  CardVariant,
+  CardHeaderProps,
+  CardTitleProps,
+  CardDescriptionProps,
+  CardContentProps,
+  CardFooterProps,
+} from "./Card";
+
+export { Input } from "./Input";
+export type { InputProps } from "./Input";
